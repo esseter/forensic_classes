3baa9bf46603881b6b05ec2de629c51c0efc007b
root <root@localhost.localdomain>
2019-02-27 19:04:05 +0100

    remove the empty lines, add the ascii art upfront, remove empty airgap

--- 2script.sh
+++ 2script.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 file=$1
+
+echo " _______ _______ _______         _______ ";
+echo "(  ____ (  ____ (  ___  |\     /(  ____ )";
+echo "| (    \| (    )| (   ) | )   ( | (    )|";
+echo "| |     | (____)| |   | | |   | | (____)|";
+echo "| | ____|     __| |   | | |   | |  _____)";
+echo "| | \_  | (\ (  | |   | | |   | | (      ";
+echo "| (___) | ) \ \_| (___) | (___) | )      ";
+echo "(_______|/   \__(_______(_______|/       ";
+echo "                                         ";
+echo "         ______                          ";
+echo "        (  __  \                         ";
+echo "        | (  \  )                        ";
+echo "        | |   ) |                        ";
+echo "        | |   | |                        ";
+echo "        | |   ) |                        ";
+echo "        | (__/  )                        ";
+echo "        (______/                         ";
+echo "                                         ";
+
 IFS="\n"
 while read line ; do
-	echo '------------------------------------------------- '
-	echo ''
 
 	# take the values of the line
 	reg_line=$(echo $line | grep "^34")
@@ -16,30 +34,44 @@ while read line ; do
 	# not updated yet
 	# output in the following format: "%M:%S"
 
-
-	if (("$timestamp_sec" < "$time_end_sec"))
+	if [ ! -z $timestamp ] # remove the error for the first line where time_end wasn't stated so the test return an error
 	then
-		overlap_sec=$(expr $time_end_sec - $timestamp_sec)
-		overlap=$(date -d @$overlap_sec +':%M:%S')
-		echo "OVERLAP with previous connection: " $overlap
-	else
-		overlap_sec=$(expr $timestamp_sec - $time_end_sec)
-		overlap=$(date -d @$overlap_sec +':%M:%S')
-		echo "AIRGAP with previous connection: " $overlap
+		if (("$timestamp_sec" < "$time_end_sec"))
+		then 
+			echo '------------------------------------------------- '
+			echo ''	
+			overlap_sec=$(expr $time_end_sec - $timestamp_sec)
+			overlap=$(date -d @$overlap_sec +'%M:%S')
+			echo "OVERLAP with previous connection: " $overlap
 
+		elif [ ! -z $timestamp ] && (("$timestamp_sec" > "$time_end_sec"))
+		then
+			echo '------------------------------------------------- '
+			echo ''
+			overlap_sec=$(expr $timestamp_sec - $time_end_sec)
+			overlap=$(date -d @$overlap_sec +'%M:%S')
+			echo "AIRGAP with previous connection: " $overlap
+		else
+			echo '------------------------------------------------- '
+			echo ''
+
+		fi
 	fi
 
 	# get the number of seconds elapsed during the duration of the connection
 	time_dur_seconds=$(echo $time_dur | awk -F':' '{print $1 * 60 * 60 + $2 * 60 + $3}')
 	
 	# We convert start date in seconds, add the duration time and convert back in correct date format for time_end
+	
 	time_end_sec=$(expr $timestamp_sec + $time_dur_seconds)
 	time_end=$(date -d @$time_end_sec +'%d/%m/%Y %H:%M:%S')
 
 	#2nd Output of our script within the terminal
-	echo "START TIME: "  $timestamp
-	echo "END TIME: " $time_end
-	echo ''
-
+	if [ ! -z $timestamp ] 
+	then
+		echo "START TIME: "  $timestamp
+		echo "END TIME: " $time_end
+		echo ''
+	fi
 done < $file
 
